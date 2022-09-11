f6b5657d44731f73f7e2b2c083eca69baa42b85e
Yazmin Perez <zoeperez333@gmail.com>
2022-09-11 15:19:32 -0700

    useReducer

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,49 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from "react";
+import topStoriesReducer from "../reducers/top-stories-reducer";
+// We import our action creators.
+import { getTopStoriesFailure, getTopStoriesSuccess } from "../actions/index";
 
-function TopStories () {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [topStories, setTopStories] = useState([]);
+// We create initial state for the useReducer hook.
+const initialState = {
+  isLoaded: false,
+  topStories: [],
+  error: null,
+};
+
+function TopStories() {
+  // We initialize the useReducer hook.
+  const [state, dispatch] = useReducer(topStoriesReducer, initialState);
 
   useEffect(() => {
-    fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-      .then(response => {
-          if (!response.ok) {
-            throw new Error(`${response.status}: ${response.statusText}`);
-          } else {
-            return response.json()
-          }
-        })
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        } else {
+          return response.json();
+        }
+      })
       .then((jsonifiedResponse) => {
-          setTopStories(jsonifiedResponse.results)
-          setIsLoaded(true)
-        })
+        // We create an action and then dispatch it.
+        const action = getTopStoriesSuccess(jsonifiedResponse.results);
+        dispatch(action);
+      })
       .catch((error) => {
-        setError(error.message)
-        setIsLoaded(true)
+        // We create an action and then dispatch it.
+        const action = getTopStoriesFailure(error.message);
+        dispatch(action);
       });
-    }, [])
-    
-    if (error) {
-      return <h1>Error: {error}</h1>;
-    } else if (!isLoaded) {
-      return <h1>...Loading...</h1>;
-    } else {
-      return (
-        <React.Fragment>
-          <h1>Top Stories</h1>
-          <ul>
-            {topStories.map((article, index) =>
-              <li key={index}>
-                <h3>{article.title}</h3>
-                <p>{article.abstract}</p>
-              </li>
-            )}
-          </ul>
-        </React.Fragment>
-      );
-    }
+  }, []);
+
+  // we destructure error, isLoaded, and topStories from the state variable.
+  const { error, isLoaded, topStories } = state;
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  } else if (!isLoaded) {
+    return <h1>...Loading...</h1>;
+  } else {
+    return (
+      <React.Fragment>
+        <h1>Top Stories</h1>
+        <ul>
+          {topStories.map((article, index) => (
+            <li key={index}>
+              <h3>{article.title}</h3>
+              <p>{article.abstract}</p>
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
   }
+}
 
 export default TopStories;
-
